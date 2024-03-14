c4dd5d59917c81725806b010f8e9f71f80f1fa9b
rishu2604 <anand.rishu310@gmail.com>
2024-03-14 12:38:05 +0530

    "Alternative way of defining routes (in comments)"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
 import HomePage from "./Pages/Home";
 import ProductsPage from "./Pages/Product";
+
+// import { RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage/>} />
+//     <Route path="/product" element={<ProductsPage/>} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
   {
@@ -11,7 +21,8 @@ const router = createBrowserRouter([
     path: '/product',
     element: <ProductsPage />
   }
-])
+]);
+
 
 function App() {
   return <RouterProvider router={router} />;
